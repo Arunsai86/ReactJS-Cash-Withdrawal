8f3a482a99bf150966129346d0c143881e15ead4
arunsai <mangarapuarunsai225@gmail.com>
2023-05-09 07:26:56 +0000

    Cash Withdrawal

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const DenominationItem = prop => {
+  const {eachItem, withdrawAmount} = prop
+  const clickButton = () => {
+    withdrawAmount(eachItem.value)
+  }
+  return (
+    <li>
+      <button className="button" type="button" onClick={clickButton}>
+        {eachItem.value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
